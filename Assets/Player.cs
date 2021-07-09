9bcac8f6dfc6ccb7727f9579d3866b764b9896b4
hktrollgames <hktrollgames@gmail.com>
2021-07-09 18:17:51 +0900

    새로 시작

---
+++ Assets/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5;
+    public float moveableDistance = 3;
+    public Transform mousePointer;
+
+    Plane plane = new Plane( new Vector3( 0, 1, 0), 0);
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (plane.Raycast(ray, out float enter))
+        {
+            Vector3 hitPoint = ray.GetPoint(enter);
+            mousePointer.position = hitPoint;
+            float distance = Vector3.Distance(hitPoint, transform.position);
+            if(distance > moveableDistance)
+            {
+                var dir = hitPoint - transform.position;
+                dir.Normalize();
+                transform.Translate(dir * speed * Time.deltaTime);
+            }
+        }
+    }
+}
