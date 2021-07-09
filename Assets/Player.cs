48b5fd07e43184d3919e8e7b7ae828bf735dfcab
hktrollgames <hktrollgames@gmail.com>
2021-07-09 20:23:35 +0900

    2단 점프 막기, 점프 시간 인스펙터에서 조절가능하게함

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,6 +13,50 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        Move();
+
+        Jump();
+    }
+
+    public AnimationCurve jumpYac;
+    private void Jump()
+    {
+        if (jumpState == JumpStateType.Jump)
+            return;
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            StartCoroutine(JumpCo());
+        }
+    }
+    public enum JumpStateType
+    {
+        Ground,
+        Jump,
+    }
+    JumpStateType jumpState;
+    public float jumpYMultiply = 1;
+    public float jumpTimeMultiply = 1;
+    private IEnumerator JumpCo()
+    {
+        jumpState = JumpStateType.Jump;
+        float jumpStartTime = Time.time;
+        float jumpDuration = jumpYac[jumpYac.length - 1].time;
+        jumpDuration *= jumpTimeMultiply;
+        float jumpEndTime = jumpStartTime + jumpDuration;
+        float sumEvaluateTime = 0;
+        while (Time.time < jumpEndTime)
+        {
+            float y = jumpYac.Evaluate(sumEvaluateTime / jumpTimeMultiply);
+            y *= jumpYMultiply;
+            transform.Translate(0, y, 0);
+            yield return null;
+            sumEvaluateTime += Time.deltaTime;
+        }
+        jumpState = JumpStateType.Ground;
+    }
+
+    private void Move()
+    {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
         if (plane.Raycast(ray, out float enter))
@@ -19,7 +64,7 @@ public class Player : MonoBehaviour
             Vector3 hitPoint = ray.GetPoint(enter);
             mousePointer.position = hitPoint;
             float distance = Vector3.Distance(hitPoint, transform.position);
-            if(distance > moveableDistance)
+            if (distance > moveableDistance)
             {
                 var dir = hitPoint - transform.position;
                 dir.Normalize();
