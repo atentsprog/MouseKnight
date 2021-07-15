9c4f91c566501115a15a132da5049f98aca7207b
hktrollgames <hktrollgames@gmail.com>
2021-07-15 17:20:36 +0900

    스폰 포인트 적용

---
+++ Assets/StageManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///  스테이지에서 발생하는 모든 이벤트 관리
+///  스테이지 부서지면 같이 부서짐.
+///  
+/// 
+/// 1. 플레이어 로드게임 시작시까지 플레이어 대기시킴
+/// 2. 스테이지 시작시 화면 밝아지게함, 
+/// 3. 몬스터 로드
+/// </summary>
+public class StageManager : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
