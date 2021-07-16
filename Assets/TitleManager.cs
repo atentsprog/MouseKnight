1334f86c23a42ea754f39e7f4c57bbd22201beb4
hktrollgames <hktrollgames@gmail.com>
2021-07-16 10:27:54 +0900

    비동기식 씬 로드방식 샘플 추가(스테이지씬이 씬이 너무 가벼워서 로드하는게 0.2초 만에 끝남, 그래서 비동기식 로드를 체감할 순 없음)

--- Assets/TitleManager.cs
+++ Assets/TitleManager.cs
@@ -1,6 +1,7 @@
 ﻿using DG.Tweening;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class TitleManager : MonoBehaviour
@@ -22,16 +23,52 @@ public class TitleManager : MonoBehaviour
         //뉴게임 누르면 어두워지게 한다음 스테이지1 로드
         Button button = GameObject.Find("TitleCanvas")
             .transform.Find("Button").GetComponent<Button>();
+
         button.AddListener(this, OnClickNewGame);
     }
 
+    public enum SceneLoadType
+    {
+        동기식,
+        비동기식
+    }
+    public SceneLoadType sceneLoadType = SceneLoadType.동기식;
     public void OnClickNewGame()
     {
+        switch (sceneLoadType)
+        {
+            case SceneLoadType.동기식:
+                LoadScene();
+                break;
+            case SceneLoadType.비동기식:
+                StartCoroutine(LoadSceneAsync());
+                break;
+        }
+    }
+
+    void LoadScene()
+    {
         blackScreen.gameObject.SetActive(true);
+        blackScreen.DOKill(); //이전 페이드가 진행중인 경우 멈추게 하기
         blackScreen.DOFade(1, 1.7f)
             .OnComplete(() =>
             {
                 UnityEngine.SceneManagement.SceneManager.LoadScene("Stage1");
             }); // 1 -> 알파 최대값 -> 어두움을 최대로 보이게 하자
+    }
+
+    IEnumerator LoadSceneAsync()
+    {
+        var progressBarImage = GameObject.Find("TitleCanvas").transform.Find("ProgressBar").GetComponent<Image>();
+        progressBarImage.gameObject.SetActive(true);
+
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync("Stage1");
+        while(asyncOperation.isDone == false)
+        {
+            float progress = asyncOperation.progress;
+            print(progress);
+            progressBarImage.fillAmount = 1 - progress;
+            yield return null;
+        }
     }
 }
